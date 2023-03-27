0ea86ed21e7790e2efa5bf41a66cbca824d785c3
Jacky Zhao <j.zhao2k19@gmail.com>
2023-03-27 22:18:36 +0200

    Add source

---
+++ src/index.js
@@ -0,0 +1,69 @@
+const core = require('@actions/core');
+const fetch = require('node-fetch');
+const tar = require('tar-fs');
+const FormData = require('form-data');
+
+let publishDir = core.getInput('publish_dir');
+let siteURL = core.getInput('site_url', { require: true });
+let dpURL = core.getInput('dp_url', { require: true });
+dpURL = dpURL.endsWith("/") ? dpURL.slice(0, -1) : dpURL;
+let refreshToken = core.getInput('refresh_token', { require: true });
+let protocols = {
+  http: core.getBooleanInput('deploy_http'),
+  hyper: core.getBooleanInput('deploy_hyper'),
+  ipfs: core.getBooleanInput('deploy_ipfs'),
+};
+
+const headers = { 
+  Authorization: `Bearer ${refreshToken}`, 
+};
+
+console.log(`Getting ready to deploy ${siteURL} to ${dpURL}`);
+
+async function run() {
+  // see if the site exists
+  const existCheckResponse = await fetch(`${dpURL}/v1/sites/${siteURL}`, { headers, method: 'GET' });
+  if (!existCheckResponse.ok) {
+    const json = await existCheckResponse.text();
+    console.log(`Couldn't verify that the site exists: ${json}`);
+    console.log("Creating a new site...");
+    const makeSiteResponse = await fetch(`${dpURL}/v1/sites`, {
+      headers: { 
+        ...headers,
+        'Content-Type': 'application/json'
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        domain: siteURL,
+        protocols,
+      })
+    });
+
+    if (!makeSiteResponse.ok) {
+      const json = await makeSiteResponse.text();
+      throw new Error(`Failed to create the site: ${json}`);
+    }
+
+    console.log(await makeSiteResponse.text());
+  }
+
+  // put request to upload the content
+  console.log("Creating tarball...");
+  const tarStream = tar.pack(publishDir);
+  const formData = new FormData();
+  formData.append('file', tarStream, siteURL);
+
+  console.log("Uploading new site content...");
+  const uploadResponse = await fetch(`${dpURL}/v1/sites/${siteURL}`, { headers, method: 'PUT', body: formData });
+  if (!uploadResponse.ok) {
+    const json = await uploadResponse.text();
+    throw new Error(`Failed to upload the contents of the site: ${json}`);
+  }
+
+  console.log("Success!");
+  const statusResponse = await fetch(`${dpURL}/v1/sites/${siteURL}`, { headers, method: 'GET' });
+  console.log(await statusResponse.text())
+}
+
+run();
+
